4e65ecb1fde792b9e889a4dfe04461091febec32
Gordon <gordonjackman@gmail.com>
2024-02-14 07:04:36 -0500

    Created the post route in api/games, but it doesnt create a game.  I don't know why the server isn't communicating with insomnia or client side.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,52 +1,123 @@
 const router = require('express').Router();
-const { Like, Games, User } = require('../models');
+const { Like, Game, User } = require('../models');
 const axios = require('axios');
-const isAuth = require('../utils/helpers');
+// const isAuth = require('../utils/helpers');
 require('dotenv').config();
 
 
 router.get('/', async (req, res) => {
-  try {
-    const headers = {
-        'Content-Type': 'application/json'
+    try {
+        const headers = {
+            'Content-Type': 'application/json'
+        };
+
+        const games = await axios.get(`https://rawg.io/api/games?key=${process.env.APIKEY}&page_size=12`, { headers });
+        const gameslist = games.data.results;
+    
+
+        const createGames = gameslist.map(data => ({
+            name: data.name,
+            image: data.background_image,
+            released: data.released,
+            rating: data.rating,
+            rawgId: data.id,
+        }));
+
+        console.log(createGames);
+
+        await Game.bulkCreate(createGames);
+
+        // await Game.create(
+        //     {
+        //         name: 'Grand Theft Auto V',
+        //         image: 'https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg',
+        //         released: '2013-09-17',
+        //         rating: 4.47,
+        //         rawgId: 3498
+        //       }
+        // );
+
+        res.render('logos', { gameslist });
+
+    } catch (err) {
+        res.status(500).json(err);
     };
-
-    const games = await axios.get(`https://rawg.io/api/games?key=${process.env.APIKEY}&page_size=15`, { headers });
-    res.render('logos', { data: games.data });
-} catch (err) {
-    res.status(500).json(err);
-};
 });
 
-router.get('/register', (req, res) => {
-    res.render('register');
+router.get('/login', (req, res) => {
+    if (isAuth) {
+        res.redirect('profiles');
+    }
+    res.render('login');
 });
 
-router.get('/profile', isAuth, async (req, res) => {
+router.get('/profiles', async (req, res) => {
     try {
         const userId = req.session.userId;
-    
+
         if (!userId) {
-          res.redirect('logos')
+            res.redirect('logos')
         };
 
         const user = await User.findbyPk(userId);
-        
+
         const likedGames = await Like.findAll({
-          where: { userId },
-          include: [{ model: Game, attributes: ['id', 'name', 'screenshot', 'genre', 'released', 'rawgId'] }],
+            where: { userId },
+            include: [{ model: Game, attributes: ['id', 'name', 'image', 'rating', 'released', 'rawgId'] }],
         });
-    
+
         const games = likedGames.map(like => like.game);
-    
-        res.render('profiles', {user: user.name, likedGames: games});
-      } catch (err) {
+
+        res.render('profiles', { user: user.name, likedGames: games });
+    } catch (err) {
         res.render('404');
-      }
+    }
 });
 
 router.get('/support', (req, res) => {
     res.render('support');
+});
+
+router.get('/games', async (req, res) => {
+    try {
+        const headers = {
+            'Content-Type': 'application/json'
+        };
+
+        const games = await axios.get(`https://rawg.io/api/games?key=${process.env.APIKEY}&page_size=12`, { headers });
+        const gameslist = games.data.results;
+        console.log(gameslist);
+        // res.json(gameslist);
+        res.render('logos', { gameslist });
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+router.get('/search', async (req, res) => {
+    try {
+        const headers = {
+            'Content-Type': 'application/json'
+        };
+
+        const response = await axios.get(`https://rawg.io/api/games?key=${process.env.APIKEY}&search=${req.query.search}&page_size=1`, { headers });
+        const games = response.data.results;
+
+        await Game.create(games.map(data => ({
+            name: data.name,
+            image: data.background_image,
+            released: data.released,
+            rating: data.rating,
+            rawgId: data.id,
+        })));
+
+        console.log('Games saved');
+
+        res.render('games', { data: games });
+    } catch (err) {
+        res.status(500).json(err);
+    };
+
 });
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,24 +1,24 @@
 const user = require('./user');
 const game = require('./game');
 
-user.belongsToMany(game, {
-  through: {
-    model: 'Like',
-    unique: false,
-  },
-  as: 'likedGames',
-  foreignKey: 'userId',
-  constraints: false,
-});
+// user.belongsToMany(game, {
+//   through: {
+//     model: 'like',
+//     unique: false,
+//   },
+//   as: 'likedGames',
+//   foreignKey: 'userId',
+//   constraints: false,
+// });
 
-game.belongsToMany(user, {
-  through: {
-    model: 'Like',
-    unique: false,
-  },
-  as: 'likingUsers',
-  foreignKey: 'likeableId',
-  constraints: false,
-});
+// game.belongsToMany(user, {
+//   through: {
+//     model: 'like',
+//     unique: false,
+//   },
+//   as: 'likingUsers',
+//   // foreignKey: ,
+//   constraints: false,
+// });
 
 module.exports = { user, game };
